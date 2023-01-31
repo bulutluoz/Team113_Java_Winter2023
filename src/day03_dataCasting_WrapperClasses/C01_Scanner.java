1db9e69f0bd03c1c1db409bd39838d54f52c7c97
ahmet bulutluoz <abbulutluoz@gmail.com>
2023-01-31 19:28:13 +0100

    3.ders sonu

---
+++ src/day03_dataCasting_WrapperClasses/C01_Scanner.java
@@ -0,0 +1,66 @@
+package day03_dataCasting_WrapperClasses;
+
+import java.util.Scanner;
+
+public class C01_Scanner {
+
+    public static void main(String[] args) {
+        //Soru 3- Kullanicidan ismini, soyismini ve yasini alip, asagidaki formmatta yazdirin.
+        //		Isminiz : John
+        //		Soyisminiz : Doe
+        //		Yasiniz : 44
+        //		Kaydiniz basariyla tamamlanmistir.
+
+
+        Scanner scan= new Scanner(System.in);
+
+        System.out.println("Lutfen isminizi giriniz");
+
+        String isim = scan.nextLine();
+
+        System.out.println("Lutfen soyisminizi yaziniz");
+
+        String soyisim= scan.nextLine();
+
+        System.out.println("Lutfen yasinizi giriniz");
+
+        double yas= scan.nextDouble();
+
+        /*
+        System.out.println("Isminiz : " + isim);
+        System.out.println("Soyisminiz : " + soyisim);
+        System.out.println("Yasiniz : " + yas);
+        System.out.println("Kaydiniz basariyla tamamlanmistir");
+         */
+
+        // Kullanici istedigimiz data turunde bilgi girisi yapmazsa
+        // kodumuzda hata olusur ve calisma durur
+        // Ilerde kullanici hatalarina karsi nasil tedbirler alacagimizi ogrenecegiz
+        // SIMDILIK kullanici biz ne diyorsak onu tam yapiyor kabul edelim
+
+        /*
+        System.out.print("Isminiz : " + isim);
+        System.out.print("Soyisminiz : " + soyisim);
+        System.out.print("Yasiniz : " + yas);
+        System.out.print("Kaydiniz basariyla tamamlanmistir");
+
+        System.out.println()'deki ln
+        yazdirma islemini yaptiktan sonra
+        alt satira gecmesini saglar
+        println yerine print yazarsak
+        yazdirdigimiz seyler yanyana yazdirilir
+
+         */
+
+        System.out.println("Isminiz : " + isim+
+                            "\nSoyisminiz : " + soyisim+
+                            "\nYasiniz : " + yas+
+                            "\nKaydiniz basariyla tamamlanmistir");
+
+        /*
+            Tek bir System.out.println() ile birden fazla satir yazdirmak isterseniz
+            String bir ifadenin icinde istedigimiz yere \n yazabilirsiniz
+         */
+
+    }
+}
---
+++ src/day03_dataCasting_WrapperClasses/C02_Scanner.java
@@ -0,0 +1,36 @@
+package day03_dataCasting_WrapperClasses;
+
+import java.util.Scanner;
+
+public class C02_Scanner {
+
+    public static void main(String[] args) {
+
+        //Soru 5- Kullanicidan ismini, soyismini ve yasini alip asagidaki formatta yazdirin.
+        //		Girilen bilgiler : J Doe, 44
+
+        Scanner scan= new Scanner(System.in);
+
+        // isterseniz tek bir aciklama ile 3 bilgiyi de alabilirsiniz
+
+        System.out.println("Lutfen isminizi, soyisminizi ve yasinizi giriniz" +
+                "\nHer bilgiden sonra ENTER'a basiniz");
+
+        char isimIlkHarf= scan.nextLine().charAt(0);
+
+        /*
+            Scanner method'larinda nextChar() yok
+            bunun icin once kullanicinin girisine gore next() veya nextLine() yazip
+            sonra .charAt(0)  yazariz
+            parantez icindeki 0 almak istedigimiz char'in index'idir
+            ve java'da index 0'dan baslar
+
+         */
+
+        String soyisim= scan.nextLine();
+        double yas = scan.nextDouble();
+
+        //		Girilen bilgiler : J Doe, 44
+        System.out.println("Girilen bilgiler : "+ isimIlkHarf +" "+soyisim+", "+ yas);
+    }
+}
